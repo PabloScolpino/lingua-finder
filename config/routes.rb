0dad543e403c413eb57694faba297ad2d19e2ed1
Pablo Scolpino <pabloscolpino@gmail.com>
2017-03-12 09:56:34 -0300

    Filtered results for current user only

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  authenticated :user do
+    root to: 'searches#index', as: :authenticated_root
+  end
   root 'home#index'
 
   resources :words
