8635320ce2b196f48219ae9db54a89a0228747e0
Pablo Scolpino <pabloscolpino@gmail.com>
2022-07-17 21:57:12 +0200

    Shorten line

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,7 +10,8 @@ module Users
       else
         # Removing extra as it can overflow some session stores
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_registration_url, alert: user.errors.full_messages.join("\n")
+        redirect_to new_user_registration_url,
+                    alert: user.errors.full_messages.join("\n")
       end
     end
     # rubocop:enable Metrics/AbcSize
